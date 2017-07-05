d5c4cba5efb34847741dd8552ea31c2b39a39283
Thiago Soares <thiagomullet@gmail.com>
2017-07-04 23:49:24 -0300

    Inclusão do Newtonsoft

--- TrabalhoFinal/IntegrationTest/IntegrationTest.cs
+++ TrabalhoFinal/IntegrationTest/IntegrationTest.cs
@@ -2,6 +2,9 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace IntegrationTest
 {
@@ -11,10 +14,25 @@ namespace IntegrationTest
         [TestMethod]
         public void TesteIntegracao()
         {
+            var basePath = "http://localhost:50396";
+            var programaPath = basePath + "/api/Produto";
+
             var client = new HttpClient();
-            var serializerProdutoAtacadista = new DataContractJsonSerializer(typeof(Atacadista.Model.Produto));
 
             var produtoAtacadista1 = new Atacadista.Model.Produto() { Id = 1, Nome = "Teste1", Valor = 1 };
+            var jsonProdutoAtacadista1 = JsonConvert.SerializeObject(produtoAtacadista1);
+
+            client.DefaultRequestHeaders.Clear();
+            //client.DefaultRequestHeaders.Accept()
+
+            client.PutAsync(programaPath + "/1", new StringContent(jsonProdutoAtacadista1)).Wait();
+
+            var jsonProdutoGravado = client.GetStringAsync(programaPath).Result;
+            var produtoGravado = JsonConvert.DeserializeObject<List<Atacadista.Model.Produto>>(jsonProdutoGravado).Single();
+
+            Assert.AreEqual(1, produtoGravado.Id);
+            Assert.AreEqual("Teste1", produtoGravado.Nome);
+            Assert.AreEqual(1, produtoGravado.Valor);
         }
     }
 }
