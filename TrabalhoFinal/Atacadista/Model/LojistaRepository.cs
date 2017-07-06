03648bfef41bfda958221aab56c78db9f2c5aca7
Thiago Soares <thiagomullet@gmail.com>
2017-07-06 00:00:50 -0300

    Helpers http replicados
    TODO: Implementar projeto comum

--- TrabalhoFinal/Atacadista/Model/LojistaRepository.cs
+++ TrabalhoFinal/Atacadista/Model/LojistaRepository.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Atacadista.Model
@@ -18,6 +21,56 @@ namespace Atacadista.Model
         public void PropostaOrcamento(Orcamento orcamento)
         {
             throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Recupera um recurso com get numa uri
+        /// </summary>
+        /// <typeparam name="T">Tipo do recurso a ser recuperado</typeparam>
+        /// <param name="uri">Caminho do recurso a ser recuperado</param>
+        /// <returns>Recurso recuperado no formado informado</returns>
+        private T Get<T>(string uri)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var json = client.GetStringAsync(uri).Result;
+                return JsonConvert.DeserializeObject<T>(json);
+            }
+        }
+
+        /// <summary>
+        /// Grava um recurso com put numa uri
+        /// </summary>
+        /// <param name="uri">Camiho do recurso a ser gravado</param>
+        /// <param name="obj">Recurso a ser gravado</param>
+        /// <returns>Resultado da gravação</returns>
+        private HttpResponseMessage Put(string uri, object obj)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var json = JsonConvert.SerializeObject(obj);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                return client.PutAsync(uri, content).Result;
+            }
+        }
+
+        /// <summary>
+        /// Grava um recurso com post numa uri
+        /// </summary>
+        /// <param name="uri">Camiho do recurso a ser gravado</param>
+        /// <param name="obj">Recurso a ser gravado</param>
+        /// <returns>Resultado da gravação</returns>
+        private HttpResponseMessage Post(string uri, object obj)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                var json = JsonConvert.SerializeObject(obj);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                return client.PostAsync(uri, content).Result;
+            }
         }
     }
 }
